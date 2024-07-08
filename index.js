7a9618ab0f82d6e178ff57d66f0a23b1db51f131
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-08 12:04:08 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF0000";
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
